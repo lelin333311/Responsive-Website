b5c87d8704477811be409e51974535aabd2f8d6e
Mohammad Sariful Haque <mohammadhaq@Mohammads-Mac-mini.local>
2026-02-16 11:09:31 +0600

    Add new feature or update existing code

--- js/script.js
+++ js/script.js
@@ -7,7 +7,45 @@ hamburger.addEventListener('click', () => {
 });
 
 document.querySelectorAll('.question').forEach(item => {
-    item.addEventListener('click', function() {
+    item.addEventListener('click', function () {
         this.classList.toggle('active');
     });
 });
+
+// Gsap
+
+// gsap.registerPlugin(SplitText, ScrollTrigger);
+
+// let split, tl;
+
+// const createSplit = () => {
+//     split && split.revert();
+//     tl && tl.revert();
+//     split = new SplitText(".wrapper p", {
+//         type: "chars"
+//     });
+
+//     tl = gsap
+//         .timeline({
+//             scrollTrigger: {
+//                 trigger: "#textSection",
+//                 start: "top top",
+//                 end: "+=150%",
+//                 pin: true,
+//                 scrub: 0.75,
+//                 markers: true
+//             }
+//         })
+//         .set(
+//             split.chars,
+//             {
+//                 color: "#ffcc66",
+//                 stagger: 0.1
+//             },
+//             0.1
+//         );
+// };
+// createSplit();
+// const debouncer = gsap.delayedCall(0.2, createSplit).pause();
+
+// window.addEventListener("resize", () => debouncer.restart(true));
